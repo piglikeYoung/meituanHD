d4e028c9a433a32af78a364f28b58cf1e00116bd
piglikeyoung <316046510@qq.com>
2015-11-06 16:34:15 +0800

    Initial Commit

---
+++ 美团HD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  美团HD
+//
+//  Created by piglikeyoung on 15/11/6.
+//  Copyright © 2015年 pikeYoung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 美团HD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  美团HD
+//
+//  Created by piglikeyoung on 15/11/6.
+//  Copyright © 2015年 pikeYoung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

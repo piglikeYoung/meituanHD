dae191440832caa8bb4fdd49fe9047312d3401e5
piglikeyoung <316046510@qq.com>
2015-11-06 17:11:01 +0800

    初始化

---
+++ 美团HD/Classes/Home/Controller/MTHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  MTHomeViewController.h
+//  美团HD
+//
+//  Created by piglikeyoung on 15/11/6.
+//  Copyright © 2015年 pikeYoung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTHomeViewController : UIViewController
+
+@end
---
+++ 美团HD/Classes/Main/Category/UIBarButtonItem+Extension.m
@@ -0,0 +1,25 @@
+//
+//  UIBarButtonItem+Extension.m
+//  黑马微博2期
+//
+//  Created by apple on 14-10-7.
+//  Copyright (c) 2014年 heima. All rights reserved.
+//
+
+#import "UIBarButtonItem+Extension.h"
+#import "UIView+Extension.h"
+
+@implementation UIBarButtonItem (Extension)
+
++ (UIBarButtonItem *)itemWithTarget:(id)target action:(SEL)action image:(NSString *)image highImage:(NSString *)highImage
+{
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [btn addTarget:target action:action forControlEvents:UIControlEventTouchUpInside];
+    // 设置图片
+    [btn setImage:[UIImage imageNamed:image] forState:UIControlStateNormal];
+    [btn setImage:[UIImage imageNamed:highImage] forState:UIControlStateHighlighted];
+    // 设置尺寸
+    btn.size = btn.currentImage.size;
+    return [[UIBarButtonItem alloc] initWithCustomView:btn];
+}
+@end

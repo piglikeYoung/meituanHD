c4f60e2f789f462342de304cd77750708043decb
piglikeyoung <316046510@qq.com>
2015-11-10 12:41:17 +0800

    自定义label，中间带划线

---
+++ 美团HD/Classes/Home/View/MTCenterLineLabel.h
@@ -0,0 +1,13 @@
+//
+//  MTCenterLineLabel.h
+//  美团HD
+//
+//  Created by piglikeyoung on 15/11/10.
+//  Copyright © 2015年 pikeYoung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTCenterLineLabel : UILabel
+
+@end
